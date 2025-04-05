e1bd1db607e0415e3ad835d62fdeec5d72d1a958
Ajao Oluwaseun <ajayholuwaseun@gmail.com>
2025-04-05 22:08:44 +0100

    updated the backend

--- portfolio_backend/api/contact.js
+++ portfolio_backend/api/contact.js
@@ -15,7 +15,7 @@ module.exports = async function handler(req, res) {
   // CORS headers
   res.setHeader('Access-Control-Allow-Credentials', true);
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,POST');
+  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, POST');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
   if (req.method === 'OPTIONS') {
